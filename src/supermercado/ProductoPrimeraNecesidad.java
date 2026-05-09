537c55634d209debe8839db5217fa726dc5466d4
NicoAGulo <nicogulo@gmail.com>
2026-05-09 13:41:46 -0300

    Ejercicio 2 - Sobrecarga

--- src/supermercado/ProductoPrimeraNecesidad.java
+++ src/supermercado/ProductoPrimeraNecesidad.java
@@ -3,8 +3,17 @@ package supermercado;
 public class ProductoPrimeraNecesidad extends Producto{
 	private double porcentajeDescuento = 10.00;
 	
+	//Forma antigua de instanciar
+	@Deprecated
 	public ProductoPrimeraNecesidad(String _nombre, double _precio, boolean _esPrecioCuidado) {
 		super(_nombre, _precio, _esPrecioCuidado);
+	}
+	
+	//Forma de ejercicio 2 de instanciar
+	public ProductoPrimeraNecesidad(String _nombre, double _precio, boolean _esPrecioCuidado, double _porcentajeDescuento) {
+		super(_nombre, _precio, _esPrecioCuidado);
+		
+		this.porcentajeDescuento = _porcentajeDescuento;
 	}
 	
 	@Override
--- test/supermercado/ProductoPrimeraNecesidadTest.java
+++ test/supermercado/ProductoPrimeraNecesidadTest.java
@@ -9,14 +9,26 @@ public class ProductoPrimeraNecesidadTest {
 
 	private ProductoPrimeraNecesidad leche;
 	
+	private ProductoPrimeraNecesidad arroz;
+	
 	@BeforeEach
 	public void setUp() {
+		
+		//producto de ejercicio 1
 		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		
+		arroz = new ProductoPrimeraNecesidad("Arroz", 5d, false, 8.00);
 	}
 	
 	@Test
 	public void testCalcularPrecio() {
 		assertEquals(new Double(7.2), leche.getPrecio());
+	}
+	
+	@Test
+	public void testCalcularPrecio2() {
+		//5.00-5.00*0.08 = 
+		assertEquals(4.6, arroz.getPrecio());
 	}
 	
 	
